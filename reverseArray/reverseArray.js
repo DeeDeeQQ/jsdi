f6004493d876b74e1024506978955f5ea90dde85
Ihor Ruzhenko <deedeeqqs@gmail.com>
2018-03-28 03:06:38 +0300

    add generating array

--- reverseArray/reverseArray.js
+++ reverseArray/reverseArray.js
@@ -1,9 +1,22 @@
-var array = [1, 2, 3, 4, 5, 6, 7];
+inputLenghtofArray = 5;
+array = [];
+var i = 0;
+while (i < inputLenghtofArray){
+    array.push(Math.floor(Math.random()*1000));
+    i++;
+}
+console.log(array);
+
+function reverse(array) {
+
 var length =  array.length;
 var result = [];
+
 for( var i=0; i <= length - 1; i++) {
     result.push(array.pop());
-    console.log(i);
 }
-console.log(result);
+return result;
+}
+
+console.log(reverse(array));
 

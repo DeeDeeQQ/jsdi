4ca8b25b69a99fc93aec1e9389e45871876cdbfa
Ihor Ruzhenko <deedeeqqs@gmail.com>
2018-04-18 00:51:03 +0300

    add route--

---
+++ react-router--v4/src/lessons/15-Buttons/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+    const Button = ({
+                     style,
+                     theme = 'orange',
+                     size,
+                     className = '',
+                     ...rest
+                 }) => {
+        const sizeClassName = size ? `button--${size}` : '';
+        return (
+            <button
+                className={`button ${className} ${sizeClassName} theme-${theme}`}
+                style={{ paddingLeft: 20, ...style}}
+                {...rest}
+            />
+        )
+    };
+
+    const App = () => (
+        <div>
+            <Button size="medium"
+                    style={{}}
+                    theme='blue'
+            >
+                <div>Blue button</div>
+            </Button>
+            <Button
+                size="large"
+                style={{}}
+            >
+                <div>Orange button</div>
+            </Button>
+        </div>
+    );
+
+    export default App;
